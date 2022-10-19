37cd549fc36e461f14aa623adb634bb0dc14ae13
Víctor M. Reyes <victor.reyes@focusservices.com>
2022-10-18 19:51:37 -0600

    chore: params and queries

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const package = require("../package.json");
+const time = Date.now();
+const now = new Date(time);
 
-router.get("/", (req, res, next) => res.send({ name: "Express with SQL" }));
+router.get("/", (req, res, next) =>
+  res.send({ name: package.name, date: now.toString() })
+);
 
+// URL con parámetros
+router.get("/users/:name/:id", (req, res, next) => {
+  res.send({ name: req.params.name, id: req.params.id });
+});
+
+// URL con query string
+router.get("/list", (req, res, next) => {
+  let numbers = [51, 92, 31, 45, 50, 96];
+
+  res.send({ number: req.query.sort ? numbers.sort() : numbers });
+});
 
 module.exports = router;
