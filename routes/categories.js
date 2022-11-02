ea2f95f91f5889d093b7a2fbcb80ddc41cd7d26d
Víctor M. Reyes <victor.reyes@focusservices.com>
2022-11-01 21:04:00 -0600

    feat: finish crud for categories

--- routes/categories.js
+++ routes/categories.js
@@ -8,12 +8,45 @@ router.get("/", async (req, res, next) => {
   res.send({ data: categories });
 });
 
+router.get("/:id", async (req, res, next) => {
+  const category = await prisma.category.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  }); // SELECT
+  res.send({ data: category });
+});
+
 router.post("/", async (req, res, next) => {
-  const newCategory = await prisma.category.create({ // INSERT
+  const newCategory = await prisma.category.create({
+    // INSERT
     data: req.body,
   });
 
   res.status(201).json(newCategory);
+});
+
+router.patch("/:id", async (req, res, next) => {
+  const updateCategory = await prisma.category.update({
+    // UPDATE
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: req.body,
+  });
+
+  res.status(200).json(updateCategory);
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const deleteCategory = await prisma.category.delete({
+    // DELETE
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+
+  res.status(200).json(deleteCategory);
 });
 
 module.exports = router;
