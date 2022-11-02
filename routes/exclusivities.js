8f66bcb451fea6b8e2cdc181c27ed893aebe63e2
Michelle Arellano <michelle.arellanofig@gmail.com>
2022-11-01 21:19:47 -0600

    feat: finish crud for exclusivities

--- routes/exclusivities.js
+++ routes/exclusivities.js
@@ -8,12 +8,45 @@ router.get("/", async (req, res, next) => {
   res.send({ data: exclusivities });
 });
 
+router.get("/:id", async (req, res, next) => {
+  const exclusivity = await prisma.exclusivity.findUnique({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  }); // SELECT
+  res.send({ data: exclusivity });
+});
+
 router.post("/", async (req, res, next) => {
-  const newExclusivity = await prisma.exclusivity.create({ // INSERT
+  const newExclusivity = await prisma.exclusivity.create({ 
+    // INSERT
     data: req.body,
   });
 
   res.status(201).json(newExclusivity);
+});
+
+router.patch("/:id", async (req, res, next) => {
+  const updateExclusivity = await prisma.exclusivity.update({
+    // UPDATE
+    where: {
+      id: parseInt(req.params.id),
+    },
+    data: req.body,
+  });
+
+  res.status(200).json(updateExclusivity);
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const deleteExclusivity = await prisma.exclusivity.delete({
+    // DELETE
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+
+  res.status(200).json(deleteExclusivity);
 });
 
 module.exports = router;
